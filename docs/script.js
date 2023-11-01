8e57c2efa21dd7186ec5b603478a83bf79c8d4a2
Pranav Mahabal <95592003+Pdada1@users.noreply.github.com>
2023-10-31 23:08:52 -0400

    -

---
+++ docs/script.js
@@ -0,0 +1,9 @@
+
+const typed = new Typed(".typed-text", {
+  strings: ['Mechatronics Engineering Student'],
+  typeSpeed: 100,
+  startDelay: 2500,
+  onComplete: (self) => { self.cursor.remove() }
+})
+
+
